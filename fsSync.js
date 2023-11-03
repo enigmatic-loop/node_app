28add874acca526402fc0ff058f30ba69c6486e9
nina <nina.sohn@gmail.com>
2023-11-02 21:42:20 -0700

    File system callback and simple http server

--- fsSync.js
+++ fsSync.js
@@ -1,5 +1,5 @@
 const { readFileSync, writeFileSync } = require('fs');
-
+console.log('start');
 const first = readFileSync('./content/first.txt', 'utf-8')
 const second = readFileSync('./content/second.txt', 'utf-8')
 
@@ -8,3 +8,6 @@ writeFileSync(
   `Here is the result: ${first}, ${second}`,
   {flag: 'a'}
 )
+console.log('done with: ');
+
+console.log('starting the next one: ');
